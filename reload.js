3663b0fa8851a64a2de5fc94f7309006d3012135
Kimmo Rundelin <kiheru@users.sourceforge.net>
2017-12-11 13:44:31 +0200

    Fixed icon name. Marked shift-click with TODO.

--- reload.js
+++ reload.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function reload(tab) {
-	// The API is just broken. There's no way to detect shift-click.
+	// TODO: There's no way to detect shift-click.
 	// The user needs to use a keyboard shortcut when a full reload is
 	// needed.
 	browser.tabs.reload(tab.id, {bypassCache: false});
